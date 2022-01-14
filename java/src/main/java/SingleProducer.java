721883a0aa3299310b9e7f9106c24f10984dc43e
I075885 <changjun.hou@sap.com>
2022-01-14 12:23:34 +0530

    add more producer code

--- java/src/main/java/SingleProducer.java
+++ java/src/main/java/SingleProducer.java
@@ -1,3 +1,4 @@
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
@@ -6,7 +7,7 @@ import java.util.Properties;
 
 public class SingleProducer {
 
-    public static void main(String[] args) {
+    public static void testProduceAndWait() {
         Properties kafkaProps = new Properties();
         kafkaProps.put("bootstrap.servers", "localhost:9092");
         kafkaProps.put("key.serializer",  "org.apache.kafka.common.serialization.StringSerializer");
@@ -23,6 +24,45 @@ public class SingleProducer {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void testProduceAndCallback() {
+        Properties kafkaProps = new Properties();
+        kafkaProps.put("bootstrap.servers", "localhost:9092");
+        kafkaProps.put("key.serializer",  "org.apache.kafka.common.serialization.StringSerializer");
+        kafkaProps.put("value.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+        KafkaProducer producer = new KafkaProducer<String, String>(kafkaProps);
+
+        ProducerRecord<String, String> record =
+                new ProducerRecord<>("CustomerCountry", "Biomedical Materials",
+                        "USA");
+        class DemoProducerCallback implements Callback {
+            @Override
+            public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                if (e != null) {
+                    e.printStackTrace();
+                } else {
+
+                }
+                System.out.println(recordMetadata.toString());
+            }
+        }
+
+        try {
+            producer.send(record, new DemoProducerCallback());
+            Thread.sleep(3000);  //wait for the producer to complete
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        //testProduceAndWait();;
+
+        testProduceAndCallback();
+
+
     }
 
 }
